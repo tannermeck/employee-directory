6db55fbb132f2992607be6cd817e73b1556214c1
TANNER MECK <steve.meck246@gmail.com>
2022-01-06 12:21:03 -0800

    header functionality complete

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,16 @@
 import { NavLink } from 'react-router-dom';
+import { useUser } from '../../context/UserContext/UserContext';
+import { signOutUser } from '../../services/users';
 import style from './header.css'
 
 export default function Header(){
+    const { user, setUser } = useUser();
+    console.log('USER', user)
+
+    const handleLogout = async() => {
+        await signOutUser()
+        setUser({})
+    }
     return (
         <section className={style.headerContainer}>
             <div className={style.leftContainer}>
@@ -10,12 +19,23 @@ export default function Header(){
                     <h1 className={style.left}>Acme Employee Directory</h1>
                 </NavLink>
             </div>
+            {user.email ? (
+                <div className={style.rightContainer}>
+                <p className={style.text}>Signed in as</p>
+                <p className={style.userText}>{`${user.email}`}</p>
+                <NavLink to='/'>
+                    <button className={style.logoutButton} onClick={handleLogout}>SIGN OUT</button>
+                </NavLink>
+            </div>
+            ) : (
             <div className={style.rightContainer}>
                 <p className={style.text}>Not Signed In</p>
                 <NavLink to='/login'>
                     <h1 className={style.login}>SIGN IN</h1>
                 </NavLink>
             </div>
+            )
+            }
         </section>
     )
 }
