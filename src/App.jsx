2eeaacd7c3eddeba253b1b913ea77bdf0bfd46bb
TANNER MECK <steve.meck246@gmail.com>
2022-01-06 12:05:48 -0800

    login working

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,33 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Home from './views/Home/Home';
 import Auth from './components/Auth/Auth.jsx'
 import Header from './components/Header/Header';
+import { UserProvider } from './context/UserContext/UserContext';
+import ConfirmEmail from './components/Auth/ConfirmEmail';
+import Profile from './views/Profile/Profile';
 
 export default function App() {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route exact path='/'>
-          <Home />
-        </Route>
-        <Route exact path='/login'>
-          <Auth />
-        </Route>
-        <Route exact path='/register'>
-          <Auth newUser />
-        </Route>
-      </Switch>
-    </Router>
+    <UserProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path='/'>
+            <Home />
+          </Route>
+          <Route exact path='/login'>
+            <Auth />
+          </Route>
+          <Route exact path='/register'>
+            <Auth newUser />
+          </Route>
+          <Route exact path='/confirm-email'>
+            <ConfirmEmail />
+          </Route>
+          <Route exact path='/create-profile'>
+            <Profile />
+          </Route>
+        </Switch>
+      </Router>
+    </UserProvider>
   )
 }
--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,18 +1,49 @@
-import { NavLink } from 'react-router-dom';
+import { useState } from 'react';
+import { NavLink, useHistory } from 'react-router-dom';
+import { useUser } from '../../context/UserContext/UserContext';
+import { signInUser, signUpUser } from '../../services/users';
 import style from './auth.css';
 
 export default function Auth({newUser = false}){
+    const history = useHistory()
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [error, setError] = useState('');
+    const { user, setUser } = useUser();
+
+    const handleLogin = async(event) => {
+        event.preventDefault();
+        try {
+            if (newUser){
+                if (!email || password.length < 8) {
+                    throw new Error('An email and password (with 8+ characters) are required.')
+                } else {
+                await signUpUser(email, password);
+                history.replace('/confirm-email')
+                }
+            } else {
+                const currentUser = await signInUser(email, password);
+                await setUser({id: currentUser.id, email: currentUser.email})
+                history.replace('/create-profile')
+            }
+
+        } catch(error){
+            setError(error.message)
+        }
+    }
     return (
         <>
             <h3>{newUser ? 'Welcome' : 'Welcome Back!'}</h3>
             <fieldset className={style.authContainer}>
                 <legend>{newUser ? 'SIGN UP' : 'SIGN IN'}</legend>
-                    <form className={style.formContainer}>
+                    <form className={style.formContainer} onSubmit={handleLogin}>
                         <label htmlFor='email'>Email:</label>
-                        <input type='email' id='email' required/>
+                        <input className={style.passwordInput} type='email' id='email' value={email} onChange={((e) => setEmail(e.target.value))} />
                         <label htmlFor='password'>Password:</label>
-                        <input type='password' id='password' required/>
+                        <input className={style.passwordInput} type='password' id='password' value={password} onChange={((e) => setPassword(e.target.value))} />
                         <button type='submit'>{newUser ? 'SIGN UP' : 'SIGN IN'}</button>
+                        <p>{error}</p>
                     </form>
             </fieldset>
             <div>
