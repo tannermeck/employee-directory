97bb4f2d0477c0b331716312ae418e4becf3cf70
TANNER MECK <steve.meck246@gmail.com>
2022-01-05 17:12:49 -0800

    signin form styling complete

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,19 +1,24 @@
+import { NavLink } from 'react-router-dom';
 import style from './auth.css';
 
 export default function Auth(){
     return (
         <>
-        <h3>Welcome Back!</h3>
-        <fieldset className={style.authContainer}>
-            <legend>SIGN IN</legend>
-                <form className={style.formContainer}>
-                    <label htmlFor='email'>Email:</label>
-                    <input type='email' id='email' required/>
-                    <label htmlFor='password'>Password:</label>
-                    <input type='password' id='password' required/>
-                    <button type='submit'>SIGN IN</button>
-                </form>
-        </fieldset>
+            <h3>Welcome Back!</h3>
+            <fieldset className={style.authContainer}>
+                <legend>SIGN IN</legend>
+                    <form className={style.formContainer}>
+                        <label htmlFor='email'>Email:</label>
+                        <input type='email' id='email' required/>
+                        <label htmlFor='password'>Password:</label>
+                        <input type='password' id='password' required/>
+                        <button type='submit'>SIGN IN</button>
+                    </form>
+            </fieldset>
+            <div className={style.register}>
+                <p>Need an account?</p>
+                <NavLink to='/register'>Sign Up</NavLink>
+            </div>
         </>
     )
 }
