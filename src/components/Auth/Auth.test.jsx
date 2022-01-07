8af47a263828f7abdda1b5bf4c1e0848f1e8763d
TANNER MECK <steve.meck246@gmail.com>
2022-01-07 12:32:18 -0800

    mock test for login passing

---
+++ src/components/Auth/Auth.test.jsx
@@ -0,0 +1,19 @@
+import {render} from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
+import Auth from './Auth';
+import { createMemoryHistory } from 'history';
+import { UserProvider } from '../../context/UserContext/UserContext';
+
+jest.mock('../../services/users');
+jest.mock('../../context/UserContext/UserContext');
+
+it('should render a login screen', () => {
+    const {container} = render(
+        <UserProvider>
+            <MemoryRouter>
+                <Auth />
+            </MemoryRouter>
+        </UserProvider>
+    )
+    expect(container).toMatchSnapshot();
+})
