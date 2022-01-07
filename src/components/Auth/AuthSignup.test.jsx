6c26a0f2060cdf18f0382d844b1a72439003489c
TANNER MECK <steve.meck246@gmail.com>
2022-01-07 12:44:21 -0800

    auth signup test working

--- src/components/Auth/AuthSignup.test.jsx
+++ src/components/Auth/AuthSignup.test.jsx
@@ -6,13 +6,13 @@ import { UserProvider } from '../../context/UserContext/UserContext';
 jest.mock('../../services/users');
 jest.mock('../../context/UserContext/UserContext');
 
-it('should render a login screen', () => {
+it('should render a signup screen', () => {
     const {container} = render(
         <UserProvider>
             <MemoryRouter>
-                <Auth />
+                <Auth newUser />
             </MemoryRouter>
         </UserProvider>
     )
-    expect(container).toMatchSnapshot();
-});
+    expect(container).toMatchSnapshot()
+})
